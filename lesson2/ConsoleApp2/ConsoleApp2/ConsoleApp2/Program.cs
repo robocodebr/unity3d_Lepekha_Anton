52bde11c49c399f82bea84bc6a6f8f426977bcbe
robocodebr <robocodebr@gmail.com>
2020-09-19 17:27:09 +0300

    Lesson2

---
+++ lesson2/ConsoleApp2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Linq.Expressions;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = "t";
+            switch (day){
+                
+
+                case "m": Console.WriteLine("Monday"); break;
+                case "t": Console.WriteLine("Thurstday"); break;
+                case "w": Console.WriteLine("Wendsday"); break;
+                case "th": Console.WriteLine("Thurstay"); break;
+                case "f": Console.WriteLine("Friday"); break;
+                default:
+                    Console.WriteLine("notday");
+                    break;
+
+            }
+
+            float var1 = 12;
+            double var2 = 13;
+            short var3 = 16;
+            if ( var1>var2&&var1>var3)
+            {
+                Console.WriteLine(var1);
+            } else if (var2 > var3 && var2 > var1)
+            {
+                Console.WriteLine(var2);
+            } else 
+            {
+                Console.WriteLine(var3);
+            }
+         
+          /*  long l = 12L;
+            byte bt = 12;
+            short sht = 13;
+            if (bt < sht) {
+                Console.WriteLine(bt);
+
+            } else if (bt >= l) {
+                Console.WriteLine(bt);
+            } else { }
+            */
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+
