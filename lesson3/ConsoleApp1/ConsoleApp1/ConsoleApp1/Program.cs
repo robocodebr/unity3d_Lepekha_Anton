c3cae01d347965efa94a373ab9968b782e29c5a2
robocodebr <robocodebr@gmail.com>
2020-09-19 16:02:12 +0300

    lesson3

---
+++ lesson3/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            List<int> list = new List<int>();
+            
+            for(int i = 0;i<=10;i++){
+               
+                list.Add(i);
+                Console.Write(i + " ");
+
+            }
+            //      string str = "lepekha_Anton19/09/2020";
+            //   int i = 0;
+
+            //   while (i < str.Length)
+            //    {
+            //       Console.WriteLine(str[i]);
+            //       i += 2;
+
+            //       i++;
+            //    }
+            // string str2 = "2008";
+            // for (int i = 0; i < str.Length; i++)
+            //  string str = "lepekha_Anton19/09/2020";
+            //for (int i = str.Length-1; i >= 0; i--)
+            // for (int i = str.Length - 1; i >= 1; i++)
+            // for (int I = 0; I < str.Length; I += 4; i++)
+            //for (int i = 1; i < str.Length; i += 5)
+            //   while  (int ii = 1; ii <= 17; ii += 4)
+            //   Console.WriteLine(ii);
+            //  i = 1;
+            // while (i <= 17)
+            // {
+            //     Console.Write(i+" ");
+            //  i+=4;
+            //   int j = -1;
+            //    do {
+            //       Console.Write(j + " ");
+            //       j -= 2;
+            //        }
+            //     while (j >= -7) ;
+            //int j += 0;
+            /*   int j = 0;
+               do
+               {
+                   Console.Write(j + " ");
+                   j += 5;
+               }
+         /*      while (j <= 20);
+
+           }
+       }
+   }
+   */
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
