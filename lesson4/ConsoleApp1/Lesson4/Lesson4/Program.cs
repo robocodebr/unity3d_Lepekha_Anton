e8bfc8afaa31d9fee263df9825a0277ada7bec2e
robocodebr <robocodebr@gmail.com>
2020-09-26 15:52:38 +0300

    lesson4

---
+++ lesson4/ConsoleApp1/Lesson4/Lesson4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+
+            List<string> l = new List<string>() { "Break", "English", "watch film", "sit in home" };
+
+            //l.Add("Break,English,watch film,sit in home);
+            foreach (string p in l)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine(l[0]);
+
+            Console.WriteLine(l.Count);
+
+            l.Clear();
+            // string l.add("Break,Englist");
+
+
+           
+            }
+        }
+    }
